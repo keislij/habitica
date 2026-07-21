12b13e7e5d289119854c7ce4244a41ca35da346a
Phillip Thelen <phillip@thelen.space>
2026-07-21 13:11:26 +0200

    clean out deferred

--- website/server/libs/payments/subscriptions.js
+++ website/server/libs/payments/subscriptions.js
@@ -362,6 +362,7 @@ async function createSubscription (data) {
       }
     }
   }
+  recipient.purchased.plan.deferred = undefined;
 
   if (group) await group.save();
   if (data.user && data.user.isModified()) await data.user.save();
